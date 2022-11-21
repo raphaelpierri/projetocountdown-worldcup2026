9180081e7addf903de29735e8196ca06d3b010bb
Raphael Pierri <rafa.pierri.silva@gmail.com>
2022-11-21 08:26:34 -0300

    Copa 2026

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 import Countdown from "./countdown.js";
 
-const tempoParaONatal = new Countdown ('20 November 2022 12:59:59 GMT-0300')
+const daysEl = document.getElementById("days")
+const hoursEl = document.getElementById("hours")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
+
+const tempoParaONatal = new Countdown ('03 June 2026 12:59:59 GMT-0300')
 
 console.log(tempoParaONatal.total)
 
-setInterval(() => {
+const el = document.createElement('div')
+el.innerHTML = setInterval(() => {
     console.log(tempoParaONatal.total)
 }, 1000)
+
